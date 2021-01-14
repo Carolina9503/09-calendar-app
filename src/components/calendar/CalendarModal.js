9d98434f03cd45d74b8a0aab2c5cce4b84103076
Carolina Daza <daza.jeymmy@gmail.com>
2021-01-14 01:18:58 -0500

    306. Validaciones del formulario

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import moment from 'moment';
 import Modal from 'react-modal';
 import DateTimePicker from 'react-datetime-picker';
+import Swal from 'sweetalert2';     
 
 
 const customStyles = {
@@ -28,6 +29,7 @@ export const CalendarModal = () => {
 
   const [ dateStart, setDateStart ] = useState(now.toDate() );
   const [ dateEnd, setDateEnd ] = useState( endPlus1.toDate() );
+  const [titleValid, setTitleValid] = useState( true )
 
   const [formValues, setFormValues] = useState({
     title: 'Evento',
@@ -36,7 +38,7 @@ export const CalendarModal = () => {
     end: endPlus1.toDate()
   });
 
-  const { title, notes } = formValues;
+  const { title, notes, start, end } = formValues;
 
   const handleInputChange = ({ target }) => {
     setFormValues({
@@ -71,7 +73,18 @@ export const CalendarModal = () => {
     //Aqui guardo los datos del formulario
     const handleSubmitForm = ( e ) => {
       e.preventDefault();
-      console.log( formValues );
+      // console.log( formValues );
+
+      const momentStart = moment( start );
+      const momentEnd = moment( end );
+
+      if ( momentStart.isSameOrAfter( momentEnd ) ) {
+        return Swal.fire('Error', 'La fecha fin debe de ser mayor a la fecha de inicio', 'error');           
+      }
+
+      if ( title.trim().length < 2 ) {
+        return setTitleValid( false )
+      }
 
     }
 
@@ -116,7 +129,7 @@ export const CalendarModal = () => {
                   <label>Titulo y notas</label>
                   <input 
                       type="text" 
-                      className="form-control"
+                      className={ `form-control ${ !titleValid && 'is-invalid' }` }
                       placeholder="Título del evento"
                       name="title"
                       value={ title }
